00d9ed370e677b034dcfaae892ab2e9029b71a51
Dulanjan Hasaranga <dulanjanhasaranga2004@gmail.com>
2026-08-07 21:35:04 +0530

    initial commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -24,7 +24,7 @@ api.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
-    if (error.response?.status === 401 && !originalRequest._retry) {
+    if ((error.response?.status === 401 || error.response?.status === 403) && !originalRequest._retry) {
       originalRequest._retry = true;
       const refreshToken = localStorage.getItem('refreshToken');
       if (refreshToken) {
